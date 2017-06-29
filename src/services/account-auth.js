d79fd61cba24ed33b0c9756cbdc6f059573b4603
Picker <450994392@qq.com>
2017-06-29 10:13:24 +0800

    feat:细化用户权限验证

--- src/services/account-auth.js
+++ src/services/account-auth.js
@@ -10,53 +10,58 @@ import Dialog from 'services/dialog';
 import router from 'services/router';
 import AccountService from 'services/stores/account';
 
-const ROLE = {
-	SUPERADMIN: 'SUPERADMIN',
-	ADMIN: 'admin',
-	USER: 'user'
-};
-
 /**
  * 判断当前是否为登录状态
  * @return {Boolean} true:登录状态,false:非登录状态
  */
-const accoutAuth = () => G.account && G.account.id && true;
+function accoutAuth() {
+	return G.account && G.account.id && true;
+}
+
+
+/**
+ * 判断角色是否为管理员
+ */
+function isAdmin(role) {
+	return role === G.ROLES.ADMIN || role === G.ROLES.SUPERADMIN;
+}
 
 /**
  * 判断当前登录账号是否为管理员
  * @return {Boolean} true:登录状态,false:非登录状态
  */
-const adminAccountAuth = () => G.account && G.account.role === ROLE.ADMIN;
+function adminAccountAuth() {
+	return G.account && G.account.role && isAdmin(G.account.role);
+}
 
 
 export default {
 	// 普通用户页面验证
 	beforeUserRouteEnter(to, from, next) {
-		if (accoutAuth()) return next();
+			if (accoutAuth()) return next();
 
-		AccountService.get().then(account => {
-			if (account && account.id) {
-				next();
-			} else {
-				router.replace('login');
-			}
-		}).catch(error => Dialog.alert(error.message));
-	},
+			AccountService.get().then(account => {
+				if (account && account.id) {
+					next();
+				} else {
+					router.replace({ name: 'login' });
+				}
+			}).catch(error => Dialog.alert(error.message));
+		},
 
-	// 管理员页面验证
-	beforeAdminRouteEnter(to, from, next) {
-		if (adminAccountAuth()) return next();
+		// 管理员页面验证
+		beforeAdminRouteEnter(to, from, next) {
+			if (adminAccountAuth()) return next();
 
-		AccountService.get().then(account => {
-			if (!account || !account.id) {
-				return router.replace('login');
-			}
-			if (account.role === ROLE.USER) {
-				return router.replace('user.profile');
-			}
-			if (account.role === ROLE.ADMIN || account.role === ROLE.SUPERADMIN) {
-				next();
-			}
-		}).catch(error => Dialog.alert(error.message));
-	}
+			AccountService.get().then(account => {
+				if (!account || !account.id) {
+					return router.replace({ name: 'login' });
+				}
+				if (!isAdmin(account.role)) {
+					router.replace({ name: 'user.profile' });
+				} else {
+					next();
+				}
+			}).catch(error => Dialog.alert(error.message));
+		}
 };
